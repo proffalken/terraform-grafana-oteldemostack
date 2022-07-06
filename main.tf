1fa81bc7346bc40081eac36c159993f484abf366
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-06 21:41:15 +0100

    Add the outputs including the various API keys

--- main.tf
+++ main.tf
@@ -13,3 +13,19 @@ resource "grafana_api_key" "management" {
   name             = "management-key"
   role             = "Admin"
 }
+
+resource "grafana_api_key" "metrics" {
+  provider = grafana.cloud
+
+  cloud_stack_slug = grafana_cloud_stack.oteldemo.slug
+  name             = "metrics-publisher"
+  role             = "MetricsPublisher"
+}
+
+resource "grafana_api_key" "plugins" {
+  provider = grafana.cloud
+
+  cloud_stack_slug = grafana_cloud_stack.oteldemo.slug
+  name             = "plugin-publisher"
+  role             = "PluginPublisher"
+}
